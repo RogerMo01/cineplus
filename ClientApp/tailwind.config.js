a5e6d9a3a06f8be69497f0e2d5b77ee277c796b1
Roger <morenoroger001@gmail.com>
2023-10-08 18:28:36 -0400

    Revert "feat: Correctly install tailwind css"
    This reverts commit 24ebd7726b8eb10566c51806665f504ee6c5c2f4.

--- ClientApp/tailwind.config.js
+++ ClientApp/tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./src/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{html,js,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
