dfd66c9109ab8c8b7043eb5936f14dbbf711bf76
Roger <morenoroger001@gmail.com>
2023-11-02 16:50:46 -0400

    feat: Add Manager page template
    - Install react-icons

---
+++ ClientApp/src/components/ManagerPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ManagerPage() {
+    return(
+        <div>
+            <h1>
+                This is manager page
+            </h1>
+        </div>
+    );
+};
+
+export default ManagerPage;
