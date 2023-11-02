c6899d48c9d01a698ced43888534c1c3f08b8f16
ClauAlvarez01 <clau200103am@gmail.com>
2023-11-02 15:53:19 -0700

    feat: Configure user-client relationship

--- Controllers/FormController.cs
+++ Controllers/FormController.cs
@@ -1,11 +1,20 @@
 using BCryptNet = BCrypt.Net.BCrypt;
 namespace cineplus.FormController;
 
+public class FormInput
+{
+    public string Nick { get; set; }
+    public string Password {get; set; }
+    public string DNI { get; set; }
+    public string CreditCard { get; set; }
+}
+
 [Route("api/form")]
 [ApiController]
 public class FormController : ControllerBase
 {
     private readonly DataContext _context;
+
     public FormController(DataContext context)
     {
         _context = context;
@@ -13,46 +22,45 @@ public class FormController : ControllerBase
 
     [HttpPost]
     [Route("register")]
-    public async Task<IActionResult> Save_Client([FromBody] Client input)
+    public async Task<IActionResult> Save_Client([FromBody] FormInput input)
     {
-        // Exclude Idc makes it auto-incremental
-        var newClient = new Client
-        {
-            Nick = input.Nick,
-            Password = input.Password,
-            DNI = input.DNI,
-            CreditCard = input.CreditCard
-        };
-
-
-        // para ver mientras se debuguea, en esta variable deben estar todos los clientes de la BD
-        // var clientes = _context.Clients.ToList(); // Consulta todos los clientes
-
-
-        // Eliminar el cliente con Idc=3
-        // var clienteABorrar = _context.Clients.FirstOrDefault(c => c.Idc == 3);
-        // if (clienteABorrar != null)
-        // {
-        //     // Borra el cliente
-        //     _context.Clients.Remove(clienteABorrar);
-        //     // Guarda los cambios en la base de datos
-        //     _context.SaveChanges();
-        // }
-
-
         if (input == null)
         {
             return BadRequest("Invalid Data");
         }
-        if (_context.Clients.Any(c => c.Nick == newClient.Nick))
+
+        if (_context.Users.Any(u => u.Nick == input.Nick))
         {
             return Conflict(new { Message = "El nombre de usuario ya está en uso" });
         }
+        
+        // Hashe password
         string salt = BCryptNet.GenerateSalt();
-        string hashed_pass = BCryptNet.HashPassword(newClient.Password, salt);
-        newClient.Password = hashed_pass;
+        string hashed_pass = BCryptNet.HashPassword(input.Password, salt);
+
+        // Exclude Idc makes it auto-incremental
+        var newUser = new User 
+        {
+            Nick = input.Nick,
+            Password = hashed_pass,
+            Salt = salt
+        };
+        _context.Users.Add(newUser);
+        await _context.SaveChangesAsync();
+
+        var newClient = new Client
+        {
+            DNI = input.DNI,
+            CreditCard = input.CreditCard,
+            UserId = newUser.UserId
+        };
         _context.Clients.Add(newClient);
         await _context.SaveChangesAsync();
+
+        // para ver mientras se debuguea, en esta variable deben estar todos los clientes de la BD
+        // var clientes = _context.Clients.ToList(); // Consulta todos los clientes
+        // var users = _context.Users.ToList();
+
         return Content("Valid Response.");
     }
 }
---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+
+namespace CineplusDB.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Nick { get; set; }
+        public string Password { get; set; }
+        public string Salt { get; set; }
+
+        public virtual Client Client { get; set; }
+    }
+}
