62234411c5cdfcb367f4df30a6efa192fef8ab3e
ClauAlvarez01 <clau200103am@gmail.com>
2023-11-06 22:08:33 -0800

    fix: Rename TicketSeller PK

--- Models/TicketSeller.cs
+++ Models/TicketSeller.cs
@@ -2,7 +2,7 @@ namespace CineplusDB.Models
 {
     public class TicketSeller
     {
-        public int SellerId { get; set; }
+        public int TicketSellerId { get; set; }
 
         public int UserId { get; set; }
         public virtual User User { get; set; }
