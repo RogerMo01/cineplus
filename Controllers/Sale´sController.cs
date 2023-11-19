9a931fb7d2bacc9fefe7b85ef3e6118fa4d01133
Kevin_Majim <96319834+MajimK@users.noreply.github.com>
2023-11-18 21:15:49 -0500

    refactor: Change input model

--- Controllers/Sale´sController.cs
+++ Controllers/Sale´sController.cs
@@ -8,13 +8,10 @@ using Microsoft.IdentityModel.Tokens;
 namespace cineplus.SalesControler;
 public class Ticket_Without_Selection
 {
-    public string Pelicula { get; set; }
+    public string MovieProgId { get; set; }
     public int butacas { get; set; }
-    public string Sala { get; set; }
-    public DateTime Horario { get; set; }
     public int cuantas { get; set; }
-    public string? Discount { get; set; }
-
+    public int Discount { get; set; }
 }
 [Route("api/sales")]
 [ApiController]
@@ -29,7 +26,7 @@ public class SalesController : Controller
 
     //[Authorize(Roles ="client")]
     [HttpPost("buy")]
-    public async Task<IActionResult> Buy_without_selection(Ticket_Without_Selection input)
+    public async Task<IActionResult> Buy_without_selection([FromBody] Ticket_Without_Selection input)
     {
         //string token = HttpContext.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
         if (input == null)
@@ -37,67 +34,58 @@ public class SalesController : Controller
             return BadRequest("Invalid Data");
         }
         //Pelicula programada que recibo de entrada
-        MovieProgramming movie = _context.ScheduledMovies
-            .Include(pp => pp.Movie)
-            .Include(pp => pp.Room)
-            .FirstOrDefault(pp => (pp.Movie.Title == input.Pelicula) && (pp.Room.Name == input.Sala))!;
+        Guid guid = new Guid(input.MovieProgId);
+        MovieProgramming movie = _context.ScheduledMovies.FirstOrDefault(mp => mp.Identifier == guid);
 
-        if (_context.Rooms.Any(c => c.Name == input.Sala))
+
+        //Verificar si tienen capacidad las salas
+        var room = _context.Rooms.FirstOrDefault(r => r.RoomId == movie.RoomId);
+        int butaca_ocupada = _context.Tickets.Include(t => t.MovieProg)
+                                            .Count(t => (t.MovieProg.MovieId == movie.MovieId) && (t.MovieProg.RoomId == movie.RoomId));
+        if (butaca_ocupada == room.SeatsCount)
         {
-            //Verificar si tienen capacidad las salas
-            var sala = _context.Rooms.FirstOrDefault(c => c.Name == input.Sala);
-            int butaca_ocupada = _context.Tickets.Include(t => t.MovieProg)
-                                                .Count(t => (t.MovieProg.MovieId == movie.MovieId) && (t.MovieProg.RoomId == movie.RoomId));
-            if (butaca_ocupada == sala.SeatsCount)
-            {
-                return Conflict(new { Message = "Sala llena" });
-            }
-            if (sala.SeatsCount - butaca_ocupada < input.cuantas)
-            {
-                return Conflict(new { Message = "Debe pedir menos cantidad" });
-            }
-            //falta incluir la tarjeta
-            Discount discount = _context.Discounts.Where(d => d.Concept == input.Discount).FirstOrDefault();
-
-            //string client_id= Obtain_ID(token);
-
-            int id = int.Parse(client_id);
-            decimal precio = movie.Price;
-            var ocupada = _context.Tickets.Where(t => t.MovieProg == movie).Select(t => t.Seat.SeatId).ToList();
-            var disponible = _context.Seats.Where(b => !ocupada.Contains(b.SeatId)).ToList();
-
-            //añade los ticket a la base de datos
-
-            for (int i = 0; i < input.cuantas; i++)
-            {
-                Ticket reserve = new Ticket();
-                reserve.DateTimeId = movie.DateTimeId;
-                reserve.MovieId = movie.MovieId;
-                reserve.RoomId = movie.RoomId;
-                reserve.SeatId = disponible.First().SeatId;
-                reserve.Price = movie.Price;
-                reserve.PricePoints = movie.PricePoints;
-
-                BuyTicket ticket_client = new BuyTicket();
-                ticket_client.ClientId = id;
-                ticket_client.DateTimeId = reserve.DateTimeId;
-                ticket_client.MovieId = reserve.MovieId;
-                ticket_client.RoomId = reserve.RoomId;
-                ticket_client.SeatId = reserve.SeatId;
-                ticket_client.DiscountId = discount.DiscountId;
-                ticket_client.Import = reserve.Price - (discount.Percent) * reserve.Price;
-                ticket_client.OnlineBuy = true;
-
-                _context.Tickets.Add(reserve);
-                disponible.RemoveAt(0);
-            }
-            await _context.SaveChangesAsync();
-            return Ok(new { Message = "Compra realizada correctamente" });
+            return Conflict(new { Message = "Sala llena" });
         }
-        else
+        if (sala.SeatsCount - butaca_ocupada < input.cuantas)
         {
-            return Conflict(new { Message = "No existe sala con ese nombre" });
+            return Conflict(new { Message = "Debe pedir menos cantidad" });
         }
+        //falta incluir la tarjeta
+        var discount = _context.Discounts.Where(d => d.DiscountId == input.Discount).FirstOrDefault();
+
+        //string client_id= Obtain_ID(token);
+        //int id = int.Parse(client_id);
+
+        var ocupada = _context.Tickets.Where(t => t.MovieProg == movie).Select(t => t.Seat.SeatId).ToList();
+        var disponible = _context.Seats.Where(b => !ocupada.Contains(b.SeatId)).ToList();
+
+        //añade los ticket a la base de datos
+
+        for (int i = 0; i < input.cuantas; i++)
+        {
+            Ticket reserve = new Ticket();
+            reserve.DateTimeId = movie.DateTimeId;
+            reserve.MovieId = movie.MovieId;
+            reserve.RoomId = movie.RoomId;
+            reserve.SeatId = disponible.First().SeatId;
+            reserve.Price = movie.Price;
+            reserve.PricePoints = movie.PricePoints;
+
+            BuyTicket ticket_client = new BuyTicket();
+            ticket_client.ClientId = id;
+            ticket_client.DateTimeId = reserve.DateTimeId;
+            ticket_client.MovieId = reserve.MovieId;
+            ticket_client.RoomId = reserve.RoomId;
+            ticket_client.SeatId = reserve.SeatId;
+            ticket_client.DiscountId = discount.DiscountId;
+            ticket_client.Import = reserve.Price - (discount.Percent) * reserve.Price;
+            ticket_client.OnlineBuy = true;
+
+            _context.Tickets.Add(reserve);
+            disponible.RemoveAt(0);
+        }
+        await _context.SaveChangesAsync();
+        return Ok(new { Message = "Compra realizada correctamente" });
     }
 
     [Authorize(Roles = "client")]
@@ -127,6 +115,8 @@ public class SalesController : Controller
 
             throw;
         }
+        double hola = 0.56666;
+        double result = Math.Round(hola, 2);
         return null;
     }
 }
