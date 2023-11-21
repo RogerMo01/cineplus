f1800c3abf7efb3a722630fcb7f73a9faf6289a7
Roger <morenoroger001@gmail.com>
2023-11-21 02:30:20 -0500

    feat: Add scroll up to footer links
    - style Footer component

--- ClientApp/src/components/Footer.tsx
+++ ClientApp/src/components/Footer.tsx
@@ -1,47 +1,75 @@
 import React from "react";
 import { NavLink } from "reactstrap";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Footer() {
-    return (
-        <div>
-            <div className="container my-5">
-                <footer className="text-center text-black">
-                    <div className="container">
-                        <section className="mt-5">
-                            <div className="row text-center d-flex justify-content-center pt-5">
-                                <div className="col-md-2">
-                                    <h6 className="text-uppercase font-weight-bold">
-                                        <NavLink tag={Link} className="text-decoration-underline" to={'/about-us'}>Sobre Nosotros</NavLink>
-                                    </h6>
-                                </div>
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
 
-                                <div className="col-md-2">
-                                    <h6 className="text-uppercase font-weight-bold">
-                                        <NavLink tag={Link} className="text-decoration-underline" to={'/catalog'}>Producto</NavLink>
-                                    </h6>
-                                </div>
+  return (
+    <div>
+      <div className="container my-5">
+        <footer className="text-center text-black">
+          <div className="container">
+            <section className="mt-5">
+              <div className="row text-center d-flex justify-content-center pt-5">
+                <div className="col-md-2">
+                  <h6 className="text-uppercase font-weight-bold">
+                    <NavLink
+                      tag={Link}
+                      onClick={scrollToTop}
+                      className="text-decoration-underline"
+                      to={"/about-us"}
+                    >
+                      Sobre Nosotros
+                    </NavLink>
+                  </h6>
+                </div>
 
-                                <div className="col-md-2">
-                                    <h6 className="text-uppercase font-weight-bold">
-                                        <NavLink tag={Link} className="text-decoration-underline" to={'/contact'}>Contacto</NavLink>
-                                    </h6>
-                                </div>
-                            </div>
-                        </section>
+                <div className="col-md-2">
+                  <h6 className="text-uppercase font-weight-bold">
+                    <NavLink
+                      tag={Link}
+                      onClick={scrollToTop}
+                      className="text-decoration-underline"
+                      to={"/movies"}
+                    >
+                      Producto
+                    </NavLink>
+                  </h6>
+                </div>
 
-                        <hr className="my-5" />
+                <div className="col-md-2">
+                  <h6 className="text-uppercase font-weight-bold">
+                    <NavLink
+                      tag={Link}
+                      onClick={scrollToTop}
+                      className="text-decoration-underline"
+                      to={"/contact"}
+                    >
+                      Contacto
+                    </NavLink>
+                  </h6>
+                </div>
+              </div>
+            </section>
 
-                    </div>
+            <hr className="my-5" />
+          </div>
 
-                    <div className="text-center p-3 style1">
-                        © 2023 Todos los derechos reservados<br/>
-                        Hecho con ❤️
-                    </div>
-                </footer>
-            </div>
-        </div>
-    );
+          <div className="text-center p-3 style1">
+            © 2023 Todos los derechos reservados
+            <br />
+            Hecho con ❤️
+          </div>
+        </footer>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
