b012e44540295af545e1b6473527d0a5a0fcc1ba
Roger <morenoroger001@gmail.com>
2023-11-20 19:00:35 -0500

    feat: Add movies posters dynamically

--- ClientApp/src/components/TopicList.tsx
+++ ClientApp/src/components/TopicList.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import { Movie }from "../types/types";
 import "./TopicList.css";
 import MovieCard from "./MovieCard";
-// @ts-ignore
-import defaultImg from '../media/default_poster.jpg'
 
 
 interface Props {
@@ -17,7 +15,7 @@ const TopicList = (props: Props) => {
       <h4>{props.topic}:</h4>
       <ul className="ul-group">
         {props.movies.map(m => (
-          <MovieCard key={m.id} movie={m} route={defaultImg} />
+          <MovieCard key={m.id} movie={m} route={`${m.id}.jpg`} />
         ))}
       </ul>
 
