a1c35d5ec47f2a12e58922a264f323c9f4b3b1fb
Roger <morenoroger001@gmail.com>
2023-11-20 19:52:35 -0500

    feat: Use <Card/> in MovieCard component

--- ClientApp/src/components/MovieCard.tsx
+++ ClientApp/src/components/MovieCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Movie } from "../types/types";
 import "./MovieCard.css";
+import { Card, CardBody, CardImg, CardSubtitle, CardTitle } from "reactstrap";
 
 interface Props {
   movie: Movie;
@@ -10,15 +11,17 @@ interface Props {
 function MovieCard(props: Props) {
   return (
     <li className="movie-card">
-      <img
-        src={props.route}
-        width={110}
-        height={170}
-        className="movie-img"
-        alt={props.movie.title}
-      />
-      <div>{props.movie.title}</div>
-      <div>•{props.movie.year}•</div>
+      <Card className="tcard">
+        <CardImg width={150} height={220} src={props.route} alt={props.movie.title} className="movie-img" />
+        <CardBody>
+          <CardTitle tag="h5">
+            {props.movie.title}
+          </CardTitle>
+          <CardSubtitle className="mb-2 text-muted" tag="h6">
+            {props.movie.year}
+          </CardSubtitle>
+        </CardBody>
+      </Card>
     </li>
   );
 }
