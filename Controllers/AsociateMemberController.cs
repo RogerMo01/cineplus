4f740df3858544efcacb30ee2c7545c7a3caf732
Kevin_Majim <96319834+MajimK@users.noreply.github.com>
2023-11-23 13:12:46 -0500

    feat: Create asociate controller

---
+++ Controllers/AsociateMemberController.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Authorization;
+using Renci.SshNet.Messages;
+using System.Security.Claims;
+
+namespace cineplus.MemberController;
+
+
+[Route("api/asociate")]
+[ApiController]
+public class AsociateMemberController : Controller
+{
+    private readonly DbContext _context;
+    public AsociateMemberController(DbContext context)
+    {
+        _context = context;
+    }
+
+    //[Authorize]
+    [HttpPost]
+    public async Task<IActionResult> Asociate(MembershipData input)
+    {
+        //var identity = HttpContext.User.Identity as ClaimsIdentity;
+        //string id = ObtainJWT.GetDataJWT(identity).Item1;
+        //string role = ObtainJWT.GetDataJWT(identity).Item2;
+        /*if (role == "client")
+        {
+            Membership member = new Membership();
+            member.FullName = input.FullName;
+            member.MemberDNI = input.MemberDNI;
+            member.Points = input.Points;
+            //member.ClientID = int.Parse(id);
+            _context.Add(member);
+            await _context.SaveChangesAsync();
+            return Ok(new { Message = "Usted ahora es nuestro socio" });
+        }*/
+        //else
+        //{
+        Membership member = new Membership();
+        member.FullName = input.FullName;
+        member.MemberDNI = input.MemberDNI;
+        member.Points = input.Points;
+        _context.Add(member);
+        await _context.SaveChangesAsync();
+        return Ok(new { Message = "Su codigo es " + member.MembershipCode });
+        //}
+    }
+}
